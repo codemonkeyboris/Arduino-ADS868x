62beb1fea37d6ffcf9087881ba892dc228e646e2
Bohan Zheng <bohan@grobo.ca>
2018-09-14 09:39:56 -0400

    Still basic stuff...

--- ads868x.cpp
+++ ads868x.cpp
@@ -26,34 +26,87 @@ ADS868x::ADS868x(int CS)
 }
 
 
-ADS868x::cmd_CLEAR_HWORD()
+void ADS868x::cmd_CLEAR_HWORD(uint16_t addr , uint16_t data)
+{
+	uint8_t i = 0;
+	uint16_t cmd_addr = _format_cmd_addr(CMDBITS_CLEAR_HWORD , addr);
+	uint16_t recv[2];
+
+	SPI.setClockDivider(SPI_CLOCK_DIV16);	//Set Clockdivider for 1MHz SPI freq.
+	SPI.setBitOrder(MSBFIRST);
+	SPI.setDataMode(SPI_MODE0);
+
+	digitalWrite(_CS,LOW);
+	recv[0] = SPI.transfer16(cmd_addr);
+	recv[1] = SPI.transfer16(data);
+	digitalWrite(_CS,HIGH);
+		
+}
+
+
+uint16_t ADS868x::cmd_READ_HWORD()
+{
+	uint8_t i = 0;
+	uint16_t cmd_addr = _format_cmd_addr(CMDBITS_READ_HWORD , addr);
+	uint16_t recv[2];
+
+	SPI.setClockDivider(SPI_CLOCK_DIV16);	//Set Clockdivider for 1MHz SPI freq.
+	SPI.setBitOrder(MSBFIRST);
+	SPI.setDataMode(SPI_MODE0);
+
+	digitalWrite(_CS,LOW);
+	recv[0] = SPI.transfer16(cmd_addr);
+	recv[1] = SPI.transfer16(data);
+	digitalWrite(_CS,HIGH);
+
+	return recv[1];
+}
+
+
+uint8_t ADS868x::cmd_READ()
+{
+	uint8_t i = 0;
+	uint16_t cmd_addr = _format_cmd_addr(CMDBITS_READ , addr);
+	uint16_t recv[2];
+
+	SPI.setClockDivider(SPI_CLOCK_DIV16);	//Set Clockdivider for 1MHz SPI freq.
+	SPI.setBitOrder(MSBFIRST);
+	SPI.setDataMode(SPI_MODE0);
+
+	digitalWrite(_CS,LOW);
+	recv[0] = SPI.transfer16(cmd_addr);
+	recv[1] = SPI.transfer16(data);
+	digitalWrite(_CS,HIGH);
+
+	return recv[1];
+
+}
+
+
+void ADS868x::cmd_WRITE(uint16_t addr , uint16_t data)
 {
 
 }
 
 
-ADS868x::cmd_READ_HWORD()
+void ADS868x::cmd_SET_HWORD(uint16_t addr , uint16_t data)
 {
 
 }
 
 
-ADS868x::cmd_READ()
+////////////////////////////////
+
+uint16_t ADS868x::_format_cmd_addr(uint8_t cmd, uint8_t addr)
 {
-
-
+	uint16_t res = (cmd | (addr > 7)) < 8;
+	res |= (addr < 1);		// last bit is the 1st bit of the address
+	return res;
 }
 
 
-ADS868x::cmd_WRITE()
-{
-
-}
 
 
-ADS868x::cmd_SET_HWORD()
-{
 
-}
 
 
--- ads868x.h
+++ ads868x.h
@@ -16,13 +16,25 @@
 
 #include <Arduino.h>
 
-// binary commands
-#define CMDBITS_CLEAR_HWORD 	0xc0000000		// 11000_xx_<9-bit address>_<16-bit data>
-#define CMDBITS_READ_HWORD 		0xc8000000		// hex(int(11001_xx_<9-bit address>_00000000_00000000 , 2)) in python
-#define CMDBITS_READ 			0x48000000 		// 01001_xx_<9-bit address>_00000000_00000000
-#define CMDBITS_SET_HWORD		0xd8000000		// 11011_xx_<9-bit address>_<16-bit data>
-												
+// register map
+#define REG_DEVICE_ID   		0x00
+#define REG_RST_PWRCTL  		0x04
+#define REG_SDI_CTL     		0x08
+#define REG_SDO_CTL     		0x0C
+#define REG_DATAOUT_CTL 		0x10
+#define REG_RANGE_SEL   		0x14	
+#define REG_ALARM      	 		0x20
+#define REG_ALARM_H_THG  		0x24
+#define REG_ALARM_L_TH  		0x28
 
+/* binary commands
+ * - the first 8 bits of a command.
+ * - the last bit is the 1st bit of the 9-bit address.
+*/
+#define CMDBITS_CLEAR_HWORD 	0xc0		// 11000_xx_<9-bit address>_<16-bit data>
+#define CMDBITS_READ_HWORD 		0xc8		// hex(int(11001_xx_<9-bit address>_00000000_00000000 , 2)) in python
+#define CMDBITS_READ 			0x48 		// 01001_xx_<9-bit address>_00000000_00000000
+#define CMDBITS_SET_HWORD		0xd8		// 11011_xx_<9-bit address>_<16-bit data>		
 
 
 class ADS868x
@@ -30,15 +42,24 @@ class ADS868x
 	public:
 		//constructor
 		ADS868x(int CS);
+		
 		// page 42 Table 5
-		cmd_CLEAR_HWORD();
-		cmd_READ_HWORD();
-		cmd_READ();
-		cmd_WRITE();
-		cmd_SET_HWORD();
+		void cmd_CLEAR_HWORD(uint16_t addr , uint16_t data);
+		uint16_t cmd_READ_HWORD();		// getting 16 bits
+		uint8_t cmd_READ();				// getting 8 bits
+		void cmd_WRITE(uint16_t addr , uint16_t data);
+		void cmd_SET_HWORD(uint16_t addr , uint16_t data);
 
+		// configuration
+		config()
+	
 	private:
+		// variables
 		int _CS;
-		int RVS;
+		int _RVS;
+		uint32_t* _recv_buf;
+
+		// functions
+		uint16_t _format_cmd_addr(uint8_t cmd, uint8_t addr);
 };
 #endif
